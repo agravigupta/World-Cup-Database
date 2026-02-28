5d6f864c63ce25784d24e4c9f2404e31313046eb
Agravi Gupta <agravigupta1119@gmail.com>
2026-02-28 13:44:50 +0530

    Created insert_data.sh
    data insertion script

---
+++ insert_data.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
+
+$PSQL "TRUNCATE games, teams RESTART IDENTITY;"
+
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  if [[ $YEAR != year ]]
+  then
+    # insert winner
+    INSERT_WINNER=$($PSQL "insert into teams(name) values('$WINNER') on conflict (name) do nothing")
+
+    # insert opponent
+    INSERT_OPPONENT=$($PSQL "insert into teams(name) values('$OPPONENT') on conflict (name) do nothing")
+
+    # get winner id
+    WINNER_ID=$($PSQL "select team_id from teams where name = '$WINNER'")
+
+    # get opponent id  
+    OPPONENT_ID=$($PSQL "select team_id from teams where name = '$OPPONENT'")
+
+    # insert game
+    INSERT_GAME=$($PSQL "insert into games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) values($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS) on conflict do nothing")
+  fi
+done
